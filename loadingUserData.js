045f3112a9700820cae62fcdbfb6b51894bdd1b9
LRPerzus <s10242179@connect.np.edu.sg>
2025-01-23 05:13:44 +0800

    backButton functionality complete

--- loadingUserData.js
+++ loadingUserData.js
@@ -1,29 +1,3 @@
-if (userData.username !== "")
-{
-    console.log("userData",userData);
-
-    // Updating the general options
-    const usernameElement = document.querySelector(".username");
-    usernameElement.textContent = userData.userName;
-
-    const rankElement = document.querySelector(".rank");
-    rankElement.textContent = "#" + userData.ranking;
-
-    const totalBeans = document.querySelector(".noOfBeans span");
-    totalBeans.textContent = userData.totalBeans;
-
-    const dateElement = document.querySelector(".upcomingDate");
-    dateElement.textContent = formatDate(userData.calanderEvent[0].date)
-
-    const upcomingEvent = document.querySelector(".eventName");
-    upcomingEvent.textContent = userData.calanderEvent[0].eventName;
-
-}
-else
-{
-    console.log("USERDATA IS EMPTY")
-}
-
 // Function to format the date as "3rd January"
 function formatDate(dateString) {
     const date = new Date(dateString);
@@ -53,3 +27,104 @@ function formatDate(dateString) {
     // Return the formatted date
     return `${day}${suffix} ${month}`;
 }
+
+
+if (userData.username !== "")
+{
+    console.log("userData",userData);
+
+    // Updating the general options
+    const usernameElement = document.querySelector(".username");
+    usernameElement.textContent = userData.userName;
+
+    const rankElement = document.querySelector(".rank");
+    rankElement.textContent = "#" + userData.ranking;
+
+    const totalBeans = document.querySelector(".noOfBeans span");
+    totalBeans.textContent = userData.totalBeans;
+
+    const dateElement = document.querySelector(".upcomingDate");
+    dateElement.textContent = formatDate(userData.calanderEvent[0].date)
+
+    const upcomingEvent = document.querySelector(".eventName");
+    upcomingEvent.textContent = userData.calanderEvent[0].eventName;
+
+}
+else
+{
+    console.log("USERDATA IS EMPTY")
+}
+
+// Now we are going to set those which have cursor on the HomePage that if they are clickable to run this function of removing
+function addClickListenerToPointerElements(containerElement) {
+    const allElements = containerElement.querySelectorAll('*');
+  
+    const elementsWithPointerCursor = Array.from(allElements).filter(element => {
+      const style = window.getComputedStyle(element);
+      return style.cursor === 'pointer';
+    });
+  
+    elementsWithPointerCursor.forEach(element => {
+      element.addEventListener('click', () => {
+        // Hide the ".userImage" and ".userText" elements
+        const userImage = document.querySelector(".userImage");
+        const userText = document.querySelector(".userText");
+  
+        if (userImage) userImage.classList.add("hidden");
+        if (userText) userText.classList.add("hidden");
+  
+        // Show the "#returnArrow" element
+        const returnArrow = document.querySelector("#returnArrow");
+        if (returnArrow) returnArrow.classList.remove("hidden");
+  
+        // Hide the "#controlBar" element
+        const controlBar = document.querySelector("#controlBar");
+        if (controlBar) controlBar.classList.add("hidden");
+      });
+    });
+}
+
+const homePageElement = document.getElementById("Homepage");
+addClickListenerToPointerElements(homePageElement);
+const controlPannel = document.getElementById("controlBar");
+addClickListenerToPointerElements(controlPannel);
+
+// Functionality of back button
+const backButton = document.getElementById("returnArrow");
+backButton.addEventListener("click", () =>{
+
+    const allMainPages = Array.from(document.querySelectorAll(".mainContent"));
+    console.log("allMainPages",allMainPages)
+    allMainPages.forEach(mainPage => {
+        if (mainPage.getAttribute("id") !== "Homepage")
+        {
+            mainPage.classList.add("hidden");
+        }
+        else
+        {
+            mainPage.classList.remove("hidden");
+        }
+    });
+
+    const userImage = document.querySelector(".userImage");
+    const userText = document.querySelector(".userText");
+
+    if (userImage) userImage.classList.remove("hidden");
+    if (userText) userText.classList.remove("hidden");
+
+    // Show the "#returnArrow" element
+    const returnArrow = document.querySelector("#returnArrow");
+    if (returnArrow) returnArrow.classList.add("hidden");
+
+    // Hide the "#controlBar" element
+    const controlBar = document.querySelector("#controlBar");
+    if (controlBar) controlBar.classList.remove("hidden");
+
+})
+
+  
+
+
+
+
+
--- map.js
+++ map.js
@@ -1,8 +1,15 @@
 function mapClick() {
+    const homePageElement = document.getElementById("Homepage");
+    homePageElement.classList.add("hidden");
+
+    const mapElement = document.getElementById("Map");
+    mapElement.classList.remove("hidden");
+
+    // Fetch the API key from apikey.txt
     fetch('apikey.txt')
         .then(response => response.text())
         .then(apiKey => {
-            // Dynamically load Google Maps API
+            // Dynamically load the Google Maps API script
             const script = document.createElement('script');
             script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey.trim()}&callback=initMap`;
             script.async = true;
@@ -15,28 +22,31 @@ function mapClick() {
 function initMap() {
     const npLocation = { lat: 1.3321, lng: 103.7741 };
 
-    // Get the .closestLocation div where the map should be created
+    // Find the .closestLocation div where the map will be created
     const closestLocationDiv = document.querySelector(".closestLocation");
 
-    // Create a div element for the map directly inside .closestLocation
-    const mapDiv = document.createElement("div");
-    mapDiv.id = "map";
-    mapDiv.style.width = "100%";
-    mapDiv.style.height = "100%";
+    // Only create the map if the map is not already present
+    if (!closestLocationDiv.querySelector('#map')) {
+        // Create the div element for the map inside .closestLocation
+        const mapDiv = document.createElement("div");
+        mapDiv.id = "map";
+        mapDiv.style.width = "100%";
+        mapDiv.style.height = "500px";
 
-    // Append the map div to .closestLocation
-    closestLocationDiv.appendChild(mapDiv);
+        // Append the new map div to .closestLocation
+        closestLocationDiv.appendChild(mapDiv);
 
-    // Initialize Google Maps inside the .closestLocation div
-    const map = new google.maps.Map(mapDiv, {
-        center: npLocation,
-        zoom: 16,
-    });
+        // Initialize the map inside the new div
+        const map = new google.maps.Map(mapDiv, {
+            center: npLocation,
+            zoom: 16,
+        });
 
-    // Add a marker at Ngee Ann Polytechnic
-    new google.maps.Marker({
-        position: npLocation,
-        map: map,
-        title: "Ngee Ann Polytechnic"
-    });
+        // Add a marker at Ngee Ann Polytechnic
+        new google.maps.Marker({
+            position: npLocation,
+            map: map,
+            title: "Ngee Ann Polytechnic"
+        });
+    }
 }
